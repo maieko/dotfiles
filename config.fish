6505fe2172be9934feb651eae3899f2ca508d9b0
Christian.Wirth <christian.wirth@kaeuferportal.de>
2015-03-20 10:16:20 +0100

    add colors to git-state-symbols

--- config.fish
+++ config.fish
@@ -12,17 +12,18 @@
   set __fish_git_prompt_showstashstate 'yes'
   set __fish_git_prompt_showuntrackedfiles 'yes'
   set __fish_git_prompt_showupstream 'yes'
-  set __fish_git_prompt_color_branch yellow
+  set __fish_git_prompt_showcolorhints 'yes'
+ # set __fish_git_prompt_color_branch yellow
   set __fish_git_prompt_color_upstream_ahead green
   set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-  set __fish_git_prompt_char_dirtystate '⚡'
-  set __fish_git_prompt_char_stagedstate '→'
-  set __fish_git_prompt_char_untrackedfiles 'Δ'
-  set __fish_git_prompt_char_stashstate '↩'
-  set __fish_git_prompt_char_upstream_ahead '+'
-  set __fish_git_prompt_char_upstream_behind '-'
+  set __fish_git_prompt_char_dirtystate (set_color red)'⚡'(set_color normal)
+  set __fish_git_prompt_char_stagedstate (set_color yellow)'→'(set_color normal)
+  set __fish_git_prompt_char_untrackedfiles (set_color red)'Δ'(set_color normal)
+  set __fish_git_prompt_char_stashstate (set_color purple)'↩'(set_color normal)
+  set __fish_git_prompt_char_upstream_ahead (set_color blue)'+'(set_color normal)
+  set __fish_git_prompt_char_upstream_behind (set_color blue)'-'(set_color normal)
 
 
 function fish_prompt
@@ -35,4 +36,8 @@ function fish_prompt
   printf '%s ' (__fish_git_prompt)
 
   set_color normal
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+  printf last_status date "+%m/%d/%y"
 end
