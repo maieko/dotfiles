37d4a2d2b64f49839651a2864c10fe35ebb6da27
Christian Windolf <christianwindolf@web.de>
2015-03-22 13:16:15 +0100

    error message if fish is not installed

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Sets Fish as the default Shell
-chsh -s /usr/bin/fish
+#####################################
+# Set Fish as the default Shell
+#####################################
+if [[ -x /usr/bin/fish ]] ; then
+  chsh -s /usr/bin/fish
+else
+  echo -e "\033[1;31mWARNING\033[0m You don't have FISH installed."
+  echo "You can still install fish afterwards."
+  echo "After installation you have to run "
+  echo -e "  $> \033[4mchsh -s /usr/bin/fish\033[0m"
+fi
 
+#####################################
 # Make symlinks to fisch config-files
+#####################################
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/aliases.fish ~/.config/fish/aliases.fish
